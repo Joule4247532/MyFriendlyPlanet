ae84396aa210ea23804d974b548f1dce575ea09a
Joule <4247532@gmail.com>
2020-04-18 15:43:44 -0400

    Asteroid Spawning

---
+++ MyFriendlyPlanet/Assets/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //References
+    public GameObject Player;
+    public GameObject Planet;
+    public GameObject Asteroid;
+
+
+    //Variables
+    private float time;
+    public float spawnRate;
+    private float cooldown;
+    public bool objDestroid;
+    
+
+    //Init
+    private void Start()
+    {
+        objDestroid = false;
+    }
+
+
+    //Runtime
+    private void Update()
+    {
+        time = Time.timeSinceLevelLoad;
+        cooldown = cooldown - Time.deltaTime;
+
+        Spawner();
+        CheckObj();
+
+    }
+
+    private void CheckObj()
+    {
+        if (objDestroid)
+        {
+            objDestroid = false;
+            SpawnAsteroid();
+        }
+    }
+
+    private void Spawner()
+    {
+        if (time % spawnRate < 0.1f && cooldown <= 0f)
+        {
+            SpawnAsteroid();
+
+            cooldown = 0.2f;
+        }
+    }
+
+    private void SpawnAsteroid()
+    {
+
+        Instantiate(Asteroid, FindPos(), new Quaternion());
+    }
+
+    private Vector3 FindPos()
+    {
+        float rdVal = Random.value - 0.5f;
+        float spawnX = Random.Range(35f,45f) * (rdVal/Mathf.Abs(rdVal)) + Player.transform.position.x;
+        rdVal = Random.value - 0.5f;
+        float spawnY = Random.Range(55f, 65f) * (rdVal / Mathf.Abs(rdVal)) + Player.transform.position.y;
+        return new Vector3(spawnX,spawnY, 0);
+    }
+}
--- MyFriendlyPlanet/Assets/Scripts/InitBody.cs
+++ MyFriendlyPlanet/Assets/Scripts/InitBody.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class InitBody : MonoBehaviour
 {
     public GameObject obj;
+    public GameObject player;
 
 
     void Awake()
     {
+        Vector2 toPlayer = player.transform.position - obj.transform.position;
         float randomScale = Random.Range(0.2f, 0.5f);
         obj.transform.localScale = new Vector3(randomScale, randomScale, 1);
-        obj.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-20f,20f), Random.Range(-20f,20f));
+        obj.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-5f,5f), Random.Range(-5f,5f)) + toPlayer.normalized * Random.Range(5f,30f);
     }
 
-    
+    private void Update()
+    {
+        if (Mathf.Abs((player.transform.position - obj.transform.position).magnitude) > 200)
+        {
+            FindObjectOfType<GameManager>().objDestroid = true;
+            Destroy(obj);
+        }
+    }
 }
