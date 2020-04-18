eb29da23367831ad501a1913f4d6852ebe70ab0a
Joule <4247532@gmail.com>
2020-04-18 14:24:25 -0400

    Random Velocity for bodies

---
+++ MyFriendlyPlanet/Assets/Scripts/InitBody.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitBody : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+
+    void Awake()
+    {
+        rb.velocity = new Vector2(Random.Range(-20f,20f), Random.Range(-20f,20f));
+    }
+
+    
+}
