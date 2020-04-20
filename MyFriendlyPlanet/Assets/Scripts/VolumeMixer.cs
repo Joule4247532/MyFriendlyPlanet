27e4131cc6a59ac01883d443fe53d1eda52740ad
Joule <4247532@gmail.com>
2020-04-20 00:47:55 -0400

    Sound Implemented

---
+++ MyFriendlyPlanet/Assets/Scripts/VolumeMixer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeMixer : MonoBehaviour
+{
+    public GameObject Tab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Tab"))
+        {
+            ToggleTab();
+        }
+    }
+
+    void ToggleTab()
+    {
+        Tab.SetActive(!Tab.activeSelf);
+        GetComponent<Image>().enabled = !GetComponent<Image>().enabled;
+    }
+}
---
+++ MyFriendlyPlanet/Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Slider Music;
+    public Slider SFX;
+
+    private void Start()
+    {
+        float value;
+        if (mixer.GetFloat("MusicVol", out value))
+        {
+            Music.value = value;
+        }
+        if (mixer.GetFloat("SFXVol", out value))
+        {
+            SFX.value = value;
+        }
+    }
+
+    private void Update()
+    { 
+        mixer.SetFloat("FXVol", SFX.value);
+        mixer.SetFloat("MusicVol", Music.value);
+    }
+}
+
